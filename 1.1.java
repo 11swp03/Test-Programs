2ca770d9056c16718295f55e7e5ef71be9af7be6
11swp03 <papallo@hartford.edu>
2022-09-09 17:08:58 -0400

    Initial commit

---
+++ 1.1.java
@@ -0,0 +1,12 @@
+// Lincoln.java Author: deez nutz
+
+public class Lincoln
+{
+    //Prints out a quote from Lincoln
+    public static void main(String[] args)
+    {
+        System.out.println("A quote by Abraham Lincoln:");
+
+        System.out.println("Whatever you are, be a good one.");
+    }
+}
