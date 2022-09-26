8c9a7f76a53e4ca9285945bcb3dc72c667108031
11jwp03 <11swp03@gmail.com>
2022-09-26 08:56:50 -0400

    added piss

--- Test/Fuck.java
+++ Test/Fuck.java
@@ -4,6 +4,9 @@ public class Fuck {
             System.out.println("Fuck" + i);
             for(int j = 0;j < 10 ;j++){
                 System.out.println("Shit" + j);
+                for(int h = 0;h < 10 ;h++){
+                    System.out.println("Piss" + h);
+                }
             }
         }
     }
