14451c7d36e6d77b206b34ac1296f308247f913e
11jwp03 <11swp03@gmail.com>
2022-09-26 08:55:50 -0400

    added gate to shit

--- Test/Fuck.java
+++ Test/Fuck.java
@@ -2,7 +2,7 @@ public class Fuck {
     public static void main(String[] args){
         for(int i = 0 ; ;i++ ){
             System.out.println("Fuck" + i);
-            for(int j = 0; ;j++){
+            for(int j = 0;j < 10 ;j++){
                 System.out.println("Shit" + j);
             }
         }
