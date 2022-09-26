19127dfb227e46f45430d6c8c79fb0a6eb479cb7
11jwp03 <11swp03@gmail.com>
2022-09-26 08:55:03 -0400

    added shit

--- Test/Fuck.java
+++ Test/Fuck.java
@@ -2,6 +2,9 @@ public class Fuck {
     public static void main(String[] args){
         for(int i = 0 ; ;i++ ){
             System.out.println("Fuck" + i);
+            for(int j = 0; ;j++){
+                System.out.println("Shit" + j);
+            }
         }
     }
 }
